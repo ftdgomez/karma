1841379e619328c2ab8ce72ca6fedf00277b4d23
Daniel Gomez <ftdgomez@gmail.com>
2022-07-02 20:41:59 +0200

    remove transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { RouteChangeHandler } from "../components/RouteChangeHandler";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <RouteChangeHandler>
-        <Component {...pageProps} />
-      </RouteChangeHandler>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
