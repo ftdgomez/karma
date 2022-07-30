afa09f87360554f32d71756a2dc52bfc5128c4fd
Daniel Gomez <ftdgomez@gmail.com>
2022-07-30 16:22:45 +0200

    change api constant

--- constants.ts
+++ constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://karma-psi.vercel.app/api';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://dainty-bavarois-cd4bf3.netlify.app/api';
