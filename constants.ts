f387d5b9cc495c1930c2a9b35c0a1bf1b1b561ab
Daniel Gomez <ftdgomez@gmail.com>
2022-08-01 20:56:28 +0200

    set api url to https

--- constants.ts
+++ constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'app.ftkarma.com/api';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://app.ftkarma.com/api';
